414e8d2b491534d100431372f709a21d4dbfe39b
ESTEVAO ANDRADE ZAGO <evozago@gmail.com>
2025-09-30 12:28:09 -0300

    Create App.simple.jsx

---
+++ webapp/src/App.simple.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">CRM Lui Bambini</h1>
+      <p className="mt-2">Teste de carregamento</p>
+    </div>
+  );
+}
+
+export default App;

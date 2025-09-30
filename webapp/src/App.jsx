0fccec8420b63db88a163d2897537bb225bffe39
ESTEVAO ANDRADE ZAGO <evozago@gmail.com>
2025-09-30 12:35:05 -0300

    Update App.jsx

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">CRM Lui Bambini</h1>
-      <p className="mt-2">Teste de carregamento</p>
-    </div>
+    <Router>
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">CRM Lui Bambini</h1>
+        <p className="mt-2">Teste de carregamento com Router</p>
+      </div>
+    </Router>
   );
 }
 
